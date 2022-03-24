dc69cda9cad1d02209141d4a5ecada9f5a9e2110
jhonynava <jhony.rona@gmail.com>
2022-03-23 21:25:59 -0600

    Fix en login

--- app/src/main/java/mx/kodemia/myfirebaseapp/view/SingInFragment.kt
+++ app/src/main/java/mx/kodemia/myfirebaseapp/view/SingInFragment.kt
@@ -1,5 +1,6 @@
 package mx.kodemia.myfirebaseapp.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.launch
 import mx.kodemia.myfirebaseapp.R
 import mx.kodemia.myfirebaseapp.databinding.FragmentSingInBinding
@@ -33,28 +35,34 @@ class SingInFragment : Fragment() {
         return root
     }
 
-    private fun initComponents() {
-        with(binding){
-            val email = tietEmail.text.toString()
-            val pass = tietPass.text.toString()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initComponents()
+    }
 
-            loginButton.setOnClickListener {
-                if (email.isNotEmpty() && pass.isNotEmpty())
-                    requireActivity().lifecycleScope.launch{
-                        val task = firebaseService.login(email, pass)
-                        task.addOnCompleteListener{
-                            if (task.isSuccessful){
-                                Toast.makeText(requireContext(), "Todo bien", Toast.LENGTH_SHORT).show()
-                            } else {
-                                Toast.makeText(requireContext(), "Mal", Toast.LENGTH_SHORT).show()
-                            }
+    private fun initComponents() {
+        binding.loginButton.setOnClickListener {
+            val email = binding.tietEmail.text.toString()
+            val pass = binding.tietPass.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                requireActivity().lifecycleScope.launch {
+                    val task = firebaseService.login(email, pass)
+                    task.addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Toast.makeText(requireContext(), "Si existe", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(requireContext(), "No existe", Toast.LENGTH_SHORT).show()
                         }
                     }
+                }
+            } else {
+                Toast.makeText(requireContext(), "Datos vacios", Toast.LENGTH_SHORT).show()
             }
+        }
 
-            singUpButton.setOnClickListener {
-                navController.navigate(R.id.action_signInFragment_to_signUpFragment)
-            }
+        binding.singUpButton.setOnClickListener {
+            navController.navigate(R.id.action_signInFragment_to_signUpFragment)
         }
     }
 }
